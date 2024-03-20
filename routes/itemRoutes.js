cbdcf9101eec99fba4ac58baa706f39f20bfbb7d
Samuel Myung <shmyung4@gmail.com>
2024-03-20 16:56:22 -0700

    Added multiple item routes

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,10 +3,37 @@
 const express = require("express");
 
 const { items } = require("../fakeDb");
+const { NotFoundError } = require("../expressError");
 const router = new express.Router();
 
 router.get("/", function(req, res) {
-  return res.json({items})
+  return res.json({items});
+});
+
+router.post("/", function(req, res) {
+  const newItem = req.body;
+  items.push(newItem);
+  return res.json({added: newItem})
+});
+
+router.get("/:name", function(req, res){
+  for (let item of items){
+    if (item.name === req.params.name){
+      return res.json(item)
+    }
+  }
+  throw new NotFoundError("Item does not exist.")
+});
+
+router.patch("/:name", function(req, res) {
+  for (let item of items){
+    if (item.name === req.params.name){
+      item.name = req.body.name;
+      item.price = req.body.price;
+      return res.json({updated: item});
+    }
+  }
+  throw new NotFoundError("Item does not exist.")
 });
 
 module.exports = router;
